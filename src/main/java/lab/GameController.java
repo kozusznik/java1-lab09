4bbef6da754e408e81611b9537478ad85f18417b
Jan Kožusznik <jan@kozusznik.cz>
2021-11-02 11:22:20 +0100

    Solution for lab08 - use CSV

--- src/main/java/lab/GameController.java
+++ src/main/java/lab/GameController.java
@@ -1,11 +1,26 @@
 	package lab;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+
 import javafx.animation.AnimationTimer;
 import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
 
 public class GameController {
 
@@ -26,12 +41,22 @@ public class GameController {
 	@FXML
 	private Label hits;
 	
+	@FXML 
+	private ListView<Score> scoreList;
+	
+	@FXML
+	private TextField nameTextField;
+	
+	private List<Score> highScores = new LinkedList<>();
+	
 	private AnimationTimer animationTimer;
 	
 	public GameController() {
 	}
 	
 	public void startGame() {
+		initScoreList();
+
 		this.world = new World(canvas.getWidth(), canvas.getHeight());
 		this.world.setGameListener(new GameListenerImpl());
 		//Draw scene on a separate thread to avoid blocking UI.
@@ -70,6 +95,50 @@ public class GameController {
 		world.setCannonStrength(newValue.doubleValue());
 	}
 	
+	@FXML
+	private void gameOverPressed() {
+		Score score = new Score(new Random().nextInt(11), nameTextField.getText());
+		scoreList.getItems().add(score);
+		Collections.sort(scoreList.getItems(),  new ScoreComparator().reversed());
+	}
+	
+	@FXML
+	private void highScoresPressed() {
+		Set<Score> tempScores = new HashSet<>();
+		tempScores.addAll(highScores);
+		highScores.clear();
+		highScores.addAll(tempScores);
+		Collections.sort(highScores,  new ScoreComparator().reversed());
+		initScoreList();
+	}
+
+	@FXML
+	private void savePressed() throws IOException {
+		try(PrintWriter pw = new PrintWriter(new FileWriter("high_score.csv"))){
+			for(Score score: highScores) {
+				pw.printf("%s;%d\n", score.getName(), score.getScore());
+			}
+		}
+	}
+	
+	
+	@FXML
+	private void loadPressed() throws IOException{
+		highScores.clear();
+		try(BufferedReader br = new BufferedReader(new FileReader("high_score.csv"))){
+			String line;
+			while(null != (line = br.readLine())) {
+				String [] tokens = line.split(";");
+				highScores.add(new Score(Integer.parseInt(tokens[1]), tokens[0]));
+			}
+		}
+		initScoreList();
+	}
+
+	private void initScoreList() {
+		scoreList.setItems(FXCollections.observableList(highScores));
+	}
+	
 	private final class AnimationTimerImpl extends AnimationTimer {
 		private Long previous;
 
@@ -98,4 +167,6 @@ public class GameController {
 		}
 		
 	}
+	
+	
 }
